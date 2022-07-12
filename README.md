7b2c76d71bef31faab306f62fcd644ff47e93099
RogelioClementeIMTC <ceinnova11442@innovaccion.mx>
2022-07-11 19:27:00 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RegiosTeamKickBoxingStaticWebApp
---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase2');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.apartado', { delay: 500 });
+ScrollReveal().reveal('.apartado2', { delay: 500 });
+ScrollReveal().reveal('.bloque', { delay: 500 });
+ScrollReveal().reveal('.noticia', { delay: 500 });
+
